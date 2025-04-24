9df1396e136bd7f782c7c3438afed25b1e6d77d3
Daniel Kreck <daniel.kreck@cplace.com>
2025-04-24 12:01:17 +0200

    Configure Spring security to not auto-redirect all requests to GH

--- src/main/java/examples/oauth2webclient/Oauth2webclientApplication.java
+++ src/main/java/examples/oauth2webclient/Oauth2webclientApplication.java
@@ -2,6 +2,10 @@ package examples.oauth2webclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @SpringBootApplication
 public class Oauth2webclientApplication {
@@ -10,4 +14,16 @@ public class Oauth2webclientApplication {
         SpringApplication.run(Oauth2webclientApplication.class, args);
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+//                .csrf(csrf -> csrf.disable())  // Disable CSRF protection
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/login/**", "/error**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Client(Customizer.withDefaults());
+
+        return http.build();
+    }
 }
