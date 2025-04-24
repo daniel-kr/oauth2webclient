cd651741a1f9bb5ecb7a953a60c424302dd38510
Jan Eberhardt <jan.eberhardt@cplace.com>
2025-04-24 14:02:35 +0200

    Minimal working version to get an access token from github

--- src/main/java/examples/oauth2webclient/Oauth2webclientApplication.java
+++ src/main/java/examples/oauth2webclient/Oauth2webclientApplication.java
@@ -5,7 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.annotation.RegisteredOAuth2AuthorizedClient;
+import org.springframework.security.oauth2.core.OAuth2AccessToken;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class Oauth2webclientApplication {
@@ -17,13 +22,27 @@ public class Oauth2webclientApplication {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-//                .csrf(csrf -> csrf.disable())  // Disable CSRF protection
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/", "/login/**", "/error**").permitAll()
+                        .requestMatchers("/", "/login/**", "/error**", "/github/code").permitAll()
                         .anyRequest().authenticated()
                 )
                 .oauth2Client(Customizer.withDefaults());
 
         return http.build();
+    }
+
+//    @Bean
+//    public OAuth2AccessTokenResponseClient<OAuth2AuthorizationCodeGrantRequest> accessTokenResponseClient() {
+//        return new RestClientAuthorizationCodeTokenResponseClient();
+//    }
+
+
+    @RestController
+    public static class GithubController {
+        @GetMapping("/github/code")
+        public String getAuthCode(@RegisteredOAuth2AuthorizedClient("github") OAuth2AuthorizedClient client) {
+            final OAuth2AccessToken accessToken = client.getAccessToken();
+            return accessToken.getTokenValue();
+        }
     }
 }
