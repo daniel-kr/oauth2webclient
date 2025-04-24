89cd4ed434d57da7e307ce47ed7061eb1033e460
Daniel Kreck <daniel.kreck@cplace.com>
2025-04-24 11:13:27 +0200

    Initial commit

---
+++ src/main/java/examples/oauth2webclient/Oauth2webclientApplication.java
@@ -0,0 +1,13 @@
+package examples.oauth2webclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2webclientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Oauth2webclientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/examples/oauth2webclient/Oauth2webclientApplicationTests.java
@@ -0,0 +1,13 @@
+package examples.oauth2webclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2webclientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
